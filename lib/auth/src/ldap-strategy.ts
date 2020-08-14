38cd0b5acf16fdb43cc990010ffe4dc28f05619c
Nauryzbaev Alibek <NAURYZAL@bank.corp.centercredit.kz>
2020-08-14 16:12:16 +0600

    add

--- lib/auth/src/ldap-strategy.ts
+++ lib/auth/src/ldap-strategy.ts
@@ -1,40 +1,40 @@
-import * as Strategy from "passport-ldapauth";
-import { PassportStrategy, AuthModuleOptions } from "@nestjs/passport";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { Request } from "express";
-import { AuthService } from "./auth.service";
+import * as Strategy from 'passport-ldapauth'
+import { PassportStrategy, AuthModuleOptions } from '@nestjs/passport'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { Request } from 'express'
+import { AuthService } from './auth.service'
 
 @Injectable()
-export class LdapStrategy extends PassportStrategy(Strategy, "ldap") {
+export class LdapStrategy extends PassportStrategy(Strategy, 'ldap') {
   constructor(
     private readonly service: AuthService,
-    private readonly options: AuthModuleOptions
+    private readonly options: AuthModuleOptions,
   ) {
     super(
       {
         passReqToCallback: true,
         server: {
-          url: "ldap://127.0.0.1:389",
-          bindDN: "root",
-          bindCredentials: "password",
-          searchBase: "o=users,o=example.com",
-          searchFilter: "(uid={{username}})",
-          searchAttributes: ["displayName", "mail"],
+          url: 'ldap://10.15.15.11:3268',
+          bindDN: 'BCCLDAP@bank.corp.centercredit.kz',
+          bindCredentials: 'password',
+          searchBase: 'DC=bank,DC=corp,DC=centercredit,DC=kz',
+          searchFilter: '(uid={{username}})',
+          searchAttributes: ['SamAccountName'],
         },
       },
       async (req: Request, user: any, done) => {
-        req.user = user;
-        return done(null, user);
-      }
-    );
+        req.user = user
+        return done(null, user)
+      },
+    )
   }
   async validate(username: string, password: string): Promise<any> {
-    console.log(`@LocalStrategy validate`);
-    const user = await this.service.validateUser(username, password);
-    console.log(`@LocalStrategy validate ${JSON.stringify(user)}`);
-    if (!user) throw new UnauthorizedException("Unknown user");
-    return user;
+    console.log(`@ldapStrategy validate`)
+    const user = await this.service.validateUser(username, password)
+    console.log(`@ldapStrategy validate ${JSON.stringify(user)}`)
+    if (!user) throw new UnauthorizedException('Unknown user')
+    return user
   }
-  public successRedirect: string = this.options["successRedirect"]; // '/secured-page'
-  public failureRedirect: string = this.options["failureRedirect"]; // '/login-page'
+  public successRedirect: string = this.options['successRedirect'] // '/secured-page'
+  public failureRedirect: string = this.options['failureRedirect'] // '/login-page'
 }
